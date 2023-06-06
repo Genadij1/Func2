5ab8cc3369ca403ffa44576074a0132e7686ed64
Геннадий <@DESKTOP-6NN51IE>
2023-06-06 22:02:35 +0300

    ex_3-done

--- Func2/Source.cpp
+++ Func2/Source.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 void enterArr(int arr[], int SIZE);
-void firstArr(int arr[], int SIZE);
+void sortArr(int arr[], int SIZE);
 
 int main()
 {
 	srand(time(NULL));
+	int start;
+	int end;
+
 	const int SIZE = 20;
 	int arr[SIZE];
 	enterArr(arr, SIZE);
-
-	cout << "Arr: ";
+	cout << "Arr = ";
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << " |";
 	}
-
-	firstArr(arr, SIZE);
+	sortArr(arr, SIZE);
 
 	system("pause>nul");
 	return 0;
@@ -32,16 +33,40 @@ void enterArr(int arr[], int SIZE)
 		arr[i] = rand() % 40-20;
 	}
 }
-void firstArr(int arr[], int SIZE)
+void sortArr(int arr[], int SIZE)
 {
+	int start;
 	for (int i = 0; i < SIZE; i++)
 	{
-		int start = arr[0];
-		int end = arr[9];
 
 		if (arr[i] < 1)
 		{
-			start = arr[i];
+			start = i;
+			break;
 		}
+	}
+	int end;
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		if (arr[i] < 0)
+		{
+			end = i;
+			break;
+		}
+	}
+	for (int i = start; i < end - 1; i++)
+	{
+		for (int j = start; j < end - start; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j + 1], arr[j]);
+			}
+		}
+	}
+	cout <<endl<<endl<<"sort = ";
+	for (int i = start; i <= end; i++)
+	{
+		cout<<arr[i] << " |";
 	}
 }
