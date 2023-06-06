315bfd6b58ef863a70f254e5a3999c6c194da4fa
Геннадий <@DESKTOP-6NN51IE>
2023-06-06 20:39:04 +0300

    ex_1-done

--- Func2/Source.cpp
+++ Func2/Source.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 void sort(int arr[], int SIZE);
+void sort2(int arr[], int SIZE);
 
 int main()
 {
 	const int SIZE = 10;
-	int arr[SIZE]{ 5,4,3,2,1,6,7,8,9,10 };
-	sort(int arr[SIZE],int SIZE);
+	int arr[SIZE]{ 12,5,7,4,8,0,6,5,43,7 };
+	sort(arr, SIZE);
+	sort2(arr, SIZE);
+
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << " ";
@@ -18,13 +21,26 @@ int main()
 	return 0;
 }
 
-void sortArr(int arr[],int SIZE)
+void sort(int arr[], int SIZE)
 {
-	for (int i = 0; i < SIZE -1; i++)
+	for (int i = 0; i < (SIZE / 2) - 1; i++)
 	{
-		for (int j = 0; j < SIZE - i-1; j++)
+		for (int j = 0; j < (SIZE / 2) - i - 1; j++)
 		{
 			if (arr[j] < arr[j + 1])
+			{
+				swap(arr[j + 1], arr[j]);
+			}
+		}
+	}
+}
+void sort2(int arr[], int SIZE)
+{
+	for (int i = 5; i < SIZE-1; i++)
+	{
+		for (int j = 5; j < SIZE-1; j++)
+		{
+			if (arr[j] > arr[j + 1])
 			{
 				swap(arr[j + 1], arr[j]);
 			}
