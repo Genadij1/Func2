a930320b6a99a306a22d2ce04a249d4d3d587bdf
Геннадий <@DESKTOP-6NN51IE>
2023-06-06 20:09:12 +0300

    1

--- Func2/Source.cpp
+++ Func2/Source.cpp
@@ -2,13 +2,13 @@
 #include <cstdlib>
 using namespace std;
 
-void sortArr(int arr, int SIZE);
+void sort(int arr[], int SIZE);
 
 int main()
 {
 	const int SIZE = 10;
 	int arr[SIZE]{ 5,4,3,2,1,6,7,8,9,10 };
-	sortArr(arr[SIZE], SIZE);
+	sort(int arr[SIZE],int SIZE);
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << " ";
@@ -18,7 +18,7 @@ int main()
 	return 0;
 }
 
-void sortArr(int arr, int SIZE)
+void sortArr(int arr[],int SIZE)
 {
 	for (int i = 0; i < SIZE -1; i++)
 	{
